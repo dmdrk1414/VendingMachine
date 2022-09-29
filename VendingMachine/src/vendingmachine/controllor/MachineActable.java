58fdbd30ae7676837c1311d46d52856027d585d3
root <root@DESKTOP-N52NBS3.localdomain>
2022-09-29 11:16:35 +0900

    Add interface MachineActable
    자판기의 기능을 정의 해주는 interface을 등록하였습니다.
    1. inputCoin() : 동전을 넣는 함수
    2. returnCoin() : 동전을 반환
    3. registerProduct() : 제품등록
    4. deleteProduct() : 제품삭제
    5. reviseProduct() : 제품수정
    6. showProducts() : 무슨 제품이 있는지 보여준다.
    7. pickProduct() : 제품을 선택을 한다.
    
    interface를 정의한 이유는 자판기의 행위를 먼저 생각을 하고 만들어 보고싶어서 만들어 봤습니다.

--- VendingMachine/src/vendingmachine/controllor/MachineActable.java
+++ VendingMachine/src/vendingmachine/controllor/MachineActable.java
@@ -1,7 +1,17 @@
 package vendingmachine.controllor;
 
 public interface MachineActable {
-	public void insertCoin();
+	public void inputCoin();
+
 	public void returnCoin();
-	public void 
+
+	public void registerProduct();
+
+	public void deleteProduct();
+
+	public void reviseProduct();
+
+	public void showProducts();
+
+	public void pickProduct();
 }
