6d4a56fe6c643b804abce40abc85c7c5a9cedf83
root <root@DESKTOP-N52NBS3.localdomain>
2022-09-29 13:09:41 +0900

    Add Drink class
    vendingmachine에서 관리하던 음료의 이름, 가격, 수량을 Drink 클래스에서 관리하도록함.
    객체의 독립성을 유지하기 위한 노력이다.
    
    addStockOfDrink, subtractStockOfDrink : 재고관리 메서드
    isNameDrink : 이름이 같은가 객체에 메시지를 던지다.
    isEmpty : 재고가 없는가, 비었는가의 메서드이다.

---
+++ VendingMachine/src/vendingmachine/model/Drink.java
@@ -0,0 +1,42 @@
+package vendingmachine.model;
+
+public class Drink {
+	private String nameDrink;
+	private int priceDrink;
+	private int stockDrink; // stock은 재고를 말한다.
+
+	public Drink() {
+		System.out.println("Drink에 이름, 가격, 재고현황을 추가해줘");
+	}
+
+	public Drink(String nameDrink, int priceDrink, int stockDrink) {
+		this.nameDrink = nameDrink;
+		this.priceDrink = priceDrink;
+		this.stockDrink = stockDrink;
+	}
+
+	public void addStockOfDrink(int stockIncrease) {
+		this.stockDrink = this.stockDrink + stockIncrease;
+	}
+	// test
+
+	public void subtractStockOfDrink(int stockDecrease) {
+		this.stockDrink = this.stockDrink + stockDecrease;
+	}
+
+	public boolean isNameDrink(String suppositionName) {
+		if (this.nameDrink.equals(suppositionName)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean isEmpty() {
+		if (this.stockDrink == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
--- VendingMachine/src/vendingmachine/model/VendingMachine.java
+++ VendingMachine/src/vendingmachine/model/VendingMachine.java
@@ -1,13 +1,13 @@
 package vendingmachine.model;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class VendingMachine {
 	// 여기는 기계를 만드는 개념이다.
 	private String nameDrink;
 	private int priceDrink;
 	private int stockDrink; // stock은 재고를 말한다.
-	private HashMap<Drink> productManagement;
+	private ArrayList<Drink> productManagement;
 
 	public VendingMachine() {
 //		String[] nameDrinkArr = new String[] { "사이다", "콜라", "환타" };
@@ -19,10 +19,10 @@ public class VendingMachine {
 //			this.productManagement.put(nameDrinkArr[i], priceDrinkArr[i]);
 //		}
 
-		this.productManagement = new HashMap<>();
-		this.productManagement.put(new Drink("사이다", 300, 10));
-		this.productManagement.put(new Drink("콜라", 500, 10));
-		this.productManagement.put(new Drink("환타", 400, 10));
+		this.productManagement = new ArrayList<>();
+		this.productManagement.add(new Drink("사이다", 300, 10));
+		this.productManagement.add(new Drink("콜라", 500, 10));
+		this.productManagement.add(new Drink("환타", 400, 10));
 	}
 
 	public void addStockOfDrink(int stockIncrease) {
