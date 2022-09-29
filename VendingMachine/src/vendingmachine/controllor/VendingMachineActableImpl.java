50b2191ed25dd19a058449e4e2b1f2d0033c00cf
root <root@DESKTOP-N52NBS3.localdomain>
2022-09-29 13:59:17 +0900

    추가 VendingMachineActableImpl 생성

---
+++ VendingMachine/src/vendingmachine/controllor/VendingMachineActableImpl.java
@@ -0,0 +1,49 @@
+package vendingmachine.controllor;
+
+public class VendingMachineActableImpl implements VendingMachineActable {
+
+	@Override
+	public void inputCoin(vendingmachine.model.VendingMachine vendingMachine, int CoinCnt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int returnCoin(vendingmachine.model.VendingMachine vendingMachine) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void registerProduct(vendingmachine.model.VendingMachine vendingMachine, String nameDrink, int priceDrink,
+			int stockDrink) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteProduct(vendingmachine.model.VendingMachine vendingMachine, String nameDrink) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reviseProduct(vendingmachine.model.VendingMachine vendingMachine, String nameDrink, String name2Change,
+			int price2Change) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showProducts(vendingmachine.model.VendingMachine vendingMachine) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pickProduct(vendingmachine.model.VendingMachine vendingMachine, String name2Pick) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
