579e960720d8200831a6e70584a0b668d45935cd
root <root@DESKTOP-N52NBS3.localdomain>
2022-09-30 22:06:25 +0900

    수정 VendingActionView returnCoin2VendingMachine, showDrinks2user메서드추가
    returnCoin2VendingMachine : VendingActionView class안 user에게 돈을 준다.
    showDrinks2user : VendingActionView class 안 machine의 음료수를 다보여준다.

--- VendingMachine/src/vendingmachine/view/VendingActionView.java
+++ VendingMachine/src/vendingmachine/view/VendingActionView.java
@@ -16,8 +16,31 @@ public class VendingActionView {
 	}
 
 	public void putInVendingMachineCoin() {
+		System.out.print("투입할 돈: ");
 		int coinGiveUser = UI.getInt();
-		user.subtractCoinCnt(coinGiveUser);
-		machineAction.inputCoin(machine, coinGiveUser);
+		if (user.subtractCoinCnt(coinGiveUser)) {
+			machineAction.inputCoin(machine, coinGiveUser);
+			System.out.println(coinGiveUser + "원 자판기에 투입!!");
+		}
+	}
+
+	public void returnCoin2VendingMachine() {
+		int CoinsReturn = machineAction.returnCoin(this.machine);
+		if (user.addCoinCnt(CoinsReturn)) {
+			System.out.println(CoinsReturn);
+			System.out.println("자판기의 모든 돈을 반환합니다.!!!");
+		}
+	}
+
+	public void showDrinks2user() {
+		machineAction.showProducts(machine);
+	}
+
+	public static void main(String[] args) {
+		VendingActionView view = new VendingActionView();
+		view.putInVendingMachineCoin(); // 돈넣기
+		view.returnCoin2VendingMachine(); // 돈반환
+		view.putInVendingMachineCoin(); // 돈넣기
+		view.showDrinks2user(); // 음료수 보여주기.
 	}
 }
