e8b415d819f7a3bfe2e28c73b7072ac2cef196e1
root <root@DESKTOP-N52NBS3.localdomain>
2022-09-30 17:48:17 +0900

    추가 MainMenu Class
    MainMenu 클래스에서 메뉴에 관한 것을 관리해주기위해서 만들었다.
    이후 수정을 할것 같다
    
    메인메뉴| 0.Exit 1.동전넣기 2.동전반환 3.제품등록 4.제품삭제 5.제품업데이트 6.제품보기 7.제품선택 |
    버튼을 누르면 해당 함수를 콜을 해주는 식으로 수정,
    
    이후 계속 수정 예정

--- VendingMachine/src/vendingmachine/view/MainMenu.java
+++ VendingMachine/src/vendingmachine/view/MainMenu.java
@@ -1,17 +1,41 @@
 package vendingmachine.view;
 
 public class MainMenu {
-	final int MENU_COUNTER_NUMBER = 6;
-	final String MANU_SELCEK_STRING = "메인메뉴| 0.Exit 1.동전넣기 2.동전반환 3.제품등록 4.제품삭제 5.제품업데이트 6.제품보기 7.제품선택 |";
+	final private int MENU_COUNTER_NUMBER = 8;
+	final private String MANU_SELCEK_STRING = "메인메뉴| 0.Exit 1.동전넣기 2.동전반환 3.제품등록 4.제품삭제 5.제품업데이트 6.제품보기 7.제품선택 |";
 
 	public void run() {
 
 		int manuCounter = MENU_COUNTER_NUMBER;
-		int selctMenuCount = UI.returnSelectMenuNum(manuCounter);
-		System.out.println(selctMenuCount);
+		int selectMenuCount = UI.returnSelectMenuNum(manuCounter);
 		while (true) {
-			// TODO:
-//			int menuItem = 3
+			if (selectMenuCount == 0) { // menu 0.나가기
+				return;
+			}
+			switch (selectMenuCount) {
+			case 1: // menu 1.동전넣기
+
+				break;
+
+			case 2: // menu 2.동전반환
+
+				break;
+			case 3: // menu 3.제품등록
+
+				break;
+			case 4: // menu 4.제품삭제
+
+				break;
+			case 5: // menu 5.제품업데이트
+
+				break;
+			case 6: // menu 6.제품보기
+
+				break;
+			case 7: // menu 7.제품선택
+
+				break;
+			}
 		}
 	}
 
