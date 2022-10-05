d673f50246e4e267b2091a4925a1364c4b864902
root <root@DESKTOP-N52NBS3.localdomain>
2022-10-05 21:50:20 +0900

    [Add] InitGetInstance class 수정

--- VendingMachine/src/vendingmachine/controllor/inter/InitGetInstance.java
+++ VendingMachine/src/vendingmachine/controllor/inter/InitGetInstance.java
@@ -1,5 +1,7 @@
 package vendingmachine.controllor.inter;
 
+import vendingmachine.controllor.manager.ManagerControllor;
+import vendingmachine.controllor.person.PersonControllor;
 import vendingmachine.controllor.vendingmachine.VendingMachineActableImpl;
 import vendingmachine.model.Manager;
 import vendingmachine.model.Person;
@@ -9,19 +11,25 @@ public class InitGetInstance {
     public InitGetInstance() {
     }
 
-    public static Manager newInstenceManager() {
-        return new Manager();
-    }
-
     public static VendingMachine newInstenceMachine() {
         return new VendingMachine();
     }
 
-    public static VendingMachineActableImpl newInstenceMachineActableImpl() {
-        return new VendingMachineActableImpl();
-    }
-
     public static Person newInstencePerson() {
         return new Person();
+    }
+
+    public static PersonControllor newInstencePersonControllor(Person user, VendingMachine machine,
+            VendingMachineActableImpl machineAction) {
+        return new PersonControllor(user, machine, machineAction);
+    }
+
+    public static Manager newInstenceManager() {
+        return new Manager();
+    }
+
+    public static ManagerControllor newInstenceManagerControllor(Manager manager, VendingMachine machine,
+            VendingMachineActableImpl machineAction) {
+        return new ManagerControllor(manager, machine, machineAction);
     }
 }
---
+++ VendingMachine/src/vendingmachine/view/PersonMenuManager.java
@@ -0,0 +1,10 @@
+package vendingmachine.view;
+
+public class PersonMenuManager {
+    final private int MENU_COUNTER_NUMBER = 8;
+    final private String MANU_SELCEK_STRING = "메인메뉴| 0.Exit 1.동전넣기 2.동전반환 3.제품등록 4.제품삭제 5.제품업데이트 6.제품보기 7.제품선택 |";
+
+    public void run() {
+
+    }
+}
