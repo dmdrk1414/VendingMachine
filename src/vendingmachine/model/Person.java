f40ccb0ce6ee897d73ca0581e2d4dcefb50f79f8
dmdrk1414 <seungchan141414@gmail.com>
2023-01-30 21:31:15 +0900

    :memo:[Comment] 구현 기능 목록 생성 및 수정

--- src/vendingmachine/model/Person.java
+++ src/vendingmachine/model/Person.java
@@ -3,27 +3,27 @@ package vendingmachine.model;
 import develoferFuc.UI;
 
 public class Person {
-	private int coin;
+    private int coin;
 
-	public Person() {
-		this.coin = 2000;
-	}
+    public Person() {
+        this.coin = 2000;
+    }
 
-	public boolean addCoinCnt(int coin2Add) {
-		if (UI.isPositiveNum(coin2Add)) {
-			this.coin = this.coin + coin2Add;
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public boolean addCoinCnt(int coin2Add) {
+        if (UI.isPositiveNum(coin2Add)) {
+            this.coin = this.coin + coin2Add;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public boolean subtractCoinCnt(int coin2Add) {
-		if (UI.isPositiveNum(coin2Add) && this.coin > coin2Add) {
-			this.coin = this.coin - coin2Add;
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public boolean subtractCoinCnt(int coin2Add) {
+        if (UI.isPositiveNum(coin2Add) && this.coin > coin2Add) {
+            this.coin = this.coin - coin2Add;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
