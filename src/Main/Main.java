d406641f590660a6768eeefe9957381fab5c8c69
dmdrk1414 <seungchan141414@gmail.com>
2023-01-30 20:26:34 +0900

    git.ignore 수정

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,7 +2,6 @@ package Main;
 import vendingmachine.view.MainMenu;
 
 public class Main {
-
     public static void main(String[] args) {
         MainMenu mainMenu = new MainMenu();
         mainMenu.run();
